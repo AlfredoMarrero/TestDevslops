07ee144ed4f7bde11757c37499811256ebd7d551
AlfredoMarrero <a.e.marrero@hotmail.com>
2017-02-11 00:19:26 -0500

    Testing Github Desktop and learning its features
    This is a new test commit

--- TestDevslops_/ViewController.swift
+++ TestDevslops_/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print ("Say hello world.")
+        ptint ("Test 3")
     }
 
     override func didReceiveMemoryWarning() {
